97fd7bfcbd0c5b457e7fbb33428213ae84a5bd83
NapKim Math <napkimmath@gmail.com>
2025-04-12 15:51:51 -0400

    Create basic_run_queries.sql

---
+++ querying_data/basic_run_queries.sql
@@ -0,0 +1,23 @@
+/* return everything from the run table*/
+SELECT *
+FROM `napkim-math.played_data.run` ;
+
+/* return everything from the run table where the win column isn't null (the rows we care about) */
+SELECT *
+FROM `napkim-math.played_data.run` 
+WHERE win is not null;
+
+/* return everything from the run table that was in one of the first three runs */
+SELECT *
+FROM `napkim-math.played_data.run` 
+WHERE run_id <= 3;
+
+/* return everything from the run table where we played at white stake difficulty */
+SELECT *
+FROM `napkim-math.played_data.run` 
+WHERE stake_name = 'White Stake';
+
+/* return everything from the run table where we did NOT play at white stake difficulty */
+SELECT *
+FROM `napkim-math.played_data.run` 
+WHERE stake_name <> 'White Stake'; -- you can also use != as not equal to
