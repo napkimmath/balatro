3eb9e504eeed4a64a1eec14244601d9f01ad8cd6
NapKim Math <napkimmath@gmail.com>
2025-04-12 15:48:27 -0400

    Update most_common_tags.sql

--- querying_data/most_common_tags.sql
+++ querying_data/most_common_tags.sql
@@ -1,4 +1,16 @@
-/* this query returns the tags that show up the most frequently */
+/* this query returns the tags that show up the most frequently
+
+the first query just has a count by the tag name
+the second joins to the tag reference table to bring back the description as well
+*/
+
+SELECT tag_name, count(*) AS count_of_tags
+FROM `napkim-math.played_data.round`
+WHERE blind_name is not null
+AND tag_name is not null
+GROUP BY ALL
+ORDER BY 2 desc, 1 asc
+;
 
 SELECT r.tag_name, t.tag_benefit, count(*) AS count_of_tags
 FROM `napkim-math.played_data.round` AS r
