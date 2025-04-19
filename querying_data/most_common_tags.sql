5f3dac1557cc4dd712746aeaab1cb34264b1acd9
NapKim Math <napkimmath@gmail.com>
2025-04-19 15:22:21 -0400

    Update most_common_tags.sql

--- querying_data/most_common_tags.sql
+++ querying_data/most_common_tags.sql
@@ -2,6 +2,7 @@
 
 the first query just has a count by the tag name
 the second joins to the tag reference table to bring back the description as well
+the third checks what percent of the time I skip a blind to get a certain tag
 */
 
 SELECT tag_name, count(*) AS count_of_tags
@@ -19,4 +20,29 @@ USING (tag_name)
 WHERE r.blind_name is not null
 AND r.tag_name is not null
 GROUP BY ALL
-ORDER BY 3 desc, 1 asc
+ORDER BY 3 desc, 1 asc;
+
+SELECT
+  r.tag_name,
+  t.tag_benefit,
+  COUNT(*) AS count_of_tags,
+  SUM(
+  IF
+    (played = 'No - Skipped',1,0)) AS count_of_tags_skipped,
+  100*ROUND(SAFE_DIVIDE(SUM(
+      IF
+        (played = 'No - Skipped',1,0)),COUNT(*)),4) AS pct_skipped
+FROM
+  `napkim-math.played_data_views.bqv_round` AS r
+INNER JOIN
+  `reference_tables.tag_reference` t
+USING
+  (tag_name)
+WHERE
+  r.blind_name IS NOT NULL
+  AND r.tag_name IS NOT NULL
+GROUP BY
+  ALL
+ORDER BY
+  5 DESC,
+  1 ASC
