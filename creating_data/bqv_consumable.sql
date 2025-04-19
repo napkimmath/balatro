a1a8d64a95add1fd4c6f800edac37639e5f7fdcc
NapKim Math <napkimmath@gmail.com>
2025-04-19 09:11:56 -0400

    Create bqv_consumable.sql

---
+++ creating_data/bqv_consumable.sql
@@ -0,0 +1,12 @@
+/* since our run table is connected to a google sheet
+the win column will only be populated if that row is associated with a game
+so we can use that to filter our view to only the rows we care about */
+
+CREATE OR REPLACE VIEW
+  `napkim-math.played_data_views.bqv_consumable` AS (
+  SELECT
+    *
+  FROM
+    `napkim-math.played_data_raw.consumable`
+  WHERE
+    consumable_first_available_id IS NOT NULL )
