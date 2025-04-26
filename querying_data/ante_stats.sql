e156b6f1d0f320e8e8111455898f76ab7a8e870e
NapKim Math <napkimmath@gmail.com>
2025-04-26 15:28:12 -0400

    Update ante_stats.sql

--- querying_data/ante_stats.sql
+++ querying_data/ante_stats.sql
@@ -10,3 +10,18 @@ GROUP BY
   ante_id
 ORDER BY
   ante_id asc;
+
+/* how many times we played and skipped within an ante */
+
+select ante_id, 
+count(1) as total_times_played,
+sum(case when played = 'Yes' then 1
+else 0 end) as played_blind,
+sum(case when played = 'No - Skipped' then 1
+else 0 end) as skipped_blind,
+sum(case when played = 'No - Defeated Prior to Blind' then 1
+else 0 end) as skipped_defeated,
+from `played_data_views.bqv_round`
+where blind_name in ('Small Blind','Big Blind')
+group by all
+order by ante_id;
